bb461d97eb7d0a69fb8a0ee05922638989f9fc74
gauri-97 <gauri.singh2016@vitstudent.ac.in>
2017-08-15 18:52:17 +0530

    Rollin and hatin

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+var img=document.getElementById("image");
+var marginLeft=0;
+function moveRight()
+{	if(marginLeft<1000)
+	{
+		marginLeft=marginLeft+1;
+		img.style.marginLeft=marginLeft+'px';
+	}
+}
+img.onclick= function()
+{
+	var interval=setInterval(moveRight,5);
+}
