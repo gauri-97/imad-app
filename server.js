92d0c41e59ecf17ea74806e394b18583cadcf8f8
gauri-97 <30751893+gauri-97@users.noreply.github.com>
2017-09-07 19:18:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var config={
 };
 var pool=new Pool(config);
 app.use(morgan('combined'));
-app.use(bodyParser.JSON());
+app.use(bodyParser.json());
 
 var articles={
 	'article-one':
@@ -135,11 +135,11 @@ app.get('/hash/:input',function(req,res){
 	res.send(hashedString)
 });
 
-app.post('/create-user',function(Req,res){
+app.post('/create-user',function(req,res){
 	var username=req.body.username;
 	var password=req.body.password;
 
-	var salt=crypto.RandomBytes(128).toString('hex');
+	var salt=crypto.getRandomBytes(128).toString('hex');
 	var dbString=hash(password,salt);
 	pool.query('Insert into "user" (username,password) values ($1,$2)',[username,dbString],function(err,result){
 		if(err)
@@ -151,6 +151,41 @@ app.post('/create-user',function(Req,res){
 			res.send("User successfully created"+username);
 		}
 });
+});
+
+app.post('/login',function(req,res){
+	var username=req.body.username;
+	var password=req.body.password;
+
+	pool.query('select from "user" where username=$1',[username],function(err,result){
+		if(err)
+		{
+			res.status(500).send(err.toString());
+		}
+		else
+		{
+			if(result.rows.length===0)
+				{
+					res.status(403).send("Username/password is invalid");
+				}
+			else		
+			{
+				var dbString=result.rows[0].password;
+				var salt=dbString.split('$')[2];
+				var hashedPassword=hash(password,salt);
+				if(hashedPassword===dbString)
+				{
+					res.send("User successfully logged in");
+				}
+				else
+				{
+					res.status(403).send("Username/password is invalid");
+				}
+
+			}
+		}
+});
+
 });
 
 
